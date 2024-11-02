b13443958e880ae6ea7360d707ac3ec3ee32c553
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2024-11-02 14:40:24 +0100

    migrated register errors to backed

--- api/controllers/authController.ts
+++ api/controllers/authController.ts
@@ -3,21 +3,40 @@ import db from "../mysqlConnection";
 import bcrypt from "bcrypt";
 
 export const register = async (req: Request, res: Response) => {
-  const { username, password } = req.body;
-  const [findUser] = await db.query(
+  const { username, password, rePassword } = req.body;
+
+  // "rePassword is variable that checks if user types password correctly"
+
+  //Checking if username has been taken:
+  const [foundUser] = await db.query(
     "SELECT username FROM users WHERE username=(?)",
     [username]
   );
 
-  if (findUser.length > 0) {
-    res.json({ message: "Nazwa użytkownika jest już zajęte" });
+  if (foundUser.length > 0) {
+    res
+      .status(409)
+      .json({ field: "usernameErr", message: "Nazwa jest już zajęta" });
+    console.log(`"${username}" username has been used`);
+  } else if (username.length > 20) {
+    res
+      .status(409)
+      .json({ field: "usernameErr", message: "Nazwa jest za długa" });
+    console.log(`"${username}" username too long`);
+  } else if (password !== rePassword) {
+    res
+      .status(409)
+      .json({ field: "passwordErr", message: "Hasła się nie zgadzają" });
+    console.log(`"${username}" user's password doesn't match`);
   } else {
     const hashedPassword = await bcrypt.hash(password, 8);
 
-    await db.query("INSERT INTO users (username,password) VALUES (?,?)", [
+    await db.query("INSERT INTO users (username, password) VALUES (?, ?)", [
       username,
       hashedPassword,
     ]);
-    console.log(`utworzono użytkownika: ${username}`);
+    console.log(`User created: ${username}`);
   }
 };
+
+export const login = async (req: Request, res: Response) => {};

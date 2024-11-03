b4863eade62a6464c26409122978a89cccc663a5
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2024-11-03 20:09:50 +0100

    updated panel

--- client/src/components/Panel.tsx
+++ client/src/components/Panel.tsx
@@ -14,7 +14,7 @@ const Panel = ({
           <div className="absolute bg-black w-full h-full flex justify-center opacity-80" />
           <div className="w-screen h-screen fixed flex justify-center">
             <div className="fixed mt-[5%] pl-[2px] pb-[2px] flex justify-end bg-neutral-700">
-              <div className="bg-neutral-800 text-white min-h-[200px] w-[300px] md:min-w-[450px] px-4 py-4">
+              <div className="bg-neutral-800 text-white min-h-[200px] w-[300px] md:min-w-[450px] px-4 pt-4 pb-9">
                 <div className="absolute flex justify-end bg-neutral-600 top-1 right-1 transition-all ease-in duration-2">
                   <button
                     className="bg-neutral-700 w-4 h-4 hover:bg-red-500 transition-all ease-in duration-25"

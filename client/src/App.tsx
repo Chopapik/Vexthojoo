f13b7f5816edce40faebb220658964ce91efd55e
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2024-11-03 16:01:10 +0100

    added axios default api url

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,10 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import UserPage from "./pages/UserPage";
+import axios from "axios";
+
+//api url:
+axios.defaults.baseURL = "http://localhost:3000";
 
 function App() {
   return (

3813f0e3a392553659234eea89e49492b91ab702
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2024-11-04 19:23:09 +0100

    Change router name

--- api/controllers/postsController.ts
+++ api/controllers/postsController.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import db from "../mysqlConnection";
 
-export const printPosts = async (req: any, res: any) => {
+export const printAllPosts = async (req: any, res: any) => {
   const [posts] = await db.query(
     "SELECT u.username,u.avatar,p.TEXT,p.whenUpload,p.whatDevice FROM users u JOIN posts p ON p.user_id=u.id ORDER BY p.whenUpload DESC"
   );

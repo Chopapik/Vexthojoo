3fceae0cbe3d9faa77dddc891a29b840036e3353
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2024-11-10 21:19:41 +0100

    Crate CookieAuthContext

---
+++ client/src/context/CookieAuthContext.tsx
@@ -0,0 +1,57 @@
+//This context
+import axios from "axios";
+import { createContext, ReactNode, useState } from "react";
+
+interface CookieAuthTypes {
+  isLoggedIn: boolean;
+  userid: number | null;
+  username: string | null;
+  avatar: string | null;
+}
+
+export const CookieAuthContext = createContext<{
+  authData: CookieAuthTypes;
+  getUser: () => void;
+}>({
+  authData: {
+    isLoggedIn: false,
+    userid: null,
+    username: null,
+    avatar: null,
+  },
+  getUser: async () => {},
+});
+
+export const CookieAuthProvider = ({ children }: { children: ReactNode }) => {
+  const [authData, setAuthData] = useState<CookieAuthTypes>({
+    isLoggedIn: false,
+    userid: null,
+    username: null,
+    avatar: null,
+  });
+
+  const getUser = async () => {
+    try {
+      const response = await axios.get("/cookieAuth/cookieLogin", {
+        withCredentials: true,
+      });
+      setAuthData({
+        isLoggedIn: true,
+        userid: response.data.userid,
+        username: response.data.username,
+        avatar: response.data.avatar,
+      });
+      console.log(`${response.data.message}`);
+      console.log(authData);
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (err: any) {
+      console.log(`Cookie login err: ${err.response.data.message}`);
+    }
+  };
+
+  return (
+    <CookieAuthContext.Provider value={{ authData, getUser }}>
+      {children};
+    </CookieAuthContext.Provider>
+  );
+};

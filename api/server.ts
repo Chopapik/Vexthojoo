a0585e54b2f619d2607eb59245931dcae374672e
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2024-11-11 13:25:31 +0100

    Fix handle dynamic routes

--- api/server.ts
+++ api/server.ts
@@ -35,7 +35,7 @@ app.use("/user", user);
 //static from client:
 app.use(express.static(path.join(__dirname, "../client/dist")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
 });
 
 const port = process.env.PORT || 3000;

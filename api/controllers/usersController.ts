646bf6eec21fc180533d716f560cf7426f9da74e
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2024-11-11 13:44:40 +0100

    Implemented userData query

--- api/controllers/usersController.ts
+++ api/controllers/usersController.ts
@@ -8,5 +8,12 @@ export const userPage = async (req: Request, res: Response) => {
     "SELECT u.username,u.avatar,p.TEXT,p.whenUpload,p.whatDevice FROM users u JOIN posts p ON p.user_id=u.id WHERE username=? ORDER BY p.whenUpload DESC",
     [username]
   );
-  res.json(posts);
+
+  const [userData] = await db.query(
+    "SELECT username,avatar,whenRegist,whenLastLogged FROM users WHERE username=?",
+    [username]
+  );
+
+  console.log(userData);
+  res.json({ posts: posts, userData: userData });
 };

216654aa51eba7ac67fdbd86460df15b464e4a4f
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2024-11-28 14:44:28 +0100

    Implement handling errors of getting user's data

--- api/controllers/usersController.ts
+++ api/controllers/usersController.ts
@@ -1,19 +1,30 @@
 import { Request, Response } from "express";
 import db from "../mysqlConnection";
+import cookieParser from "cookie-parser";
+import jwt from "jsonwebtoken";
 
 export const userPage = async (req: Request, res: Response) => {
   const { username } = req.params;
 
-  const [posts] = await db.query(
-    "SELECT u.username,u.avatar,p.id,p.TEXT,p.whenUpload,p.whatDevice FROM users u JOIN posts p ON p.user_id=u.id WHERE username=? ORDER BY p.whenUpload DESC",
-    [username]
-  );
+  try {
+    const [posts] = await db.query(
+      "SELECT u.username,u.avatar,p.id,p.TEXT,p.whenUpload,p.whatDevice FROM users u JOIN posts p ON p.user_id=u.id WHERE username=? ORDER BY p.whenUpload DESC",
+      [username]
+    );
 
-  const [userData] = await db.query(
-    "SELECT username,avatar,whenRegist,whenLastLogged FROM users WHERE username=?",
-    [username]
-  );
+    const [userData] = await db.query(
+      "SELECT username,avatar,whenRegist,whenLastLogged FROM users WHERE username=?",
+      [username]
+    );
 
-  console.log(userData);
-  res.json({ posts: posts, userData: userData });
+    console.log(userData);
+
+    if (userData.length > 0) {
+      res.json({ posts: posts, userData: userData });
+    } else {
+      res.status(404).json({ notFoundUser: username });
+    }
+  } catch (err) {
+    res.status(500);
+  }
 };

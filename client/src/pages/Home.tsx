5defd2ac263773acebd9c23c99931cfad3483889
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2024-12-01 16:00:58 +0100

    Add first block in aside

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -124,7 +124,26 @@ const Home = () => {
             </>
           )}
         </main>
-        <aside className="w-full lg:w-1/2 2xl:1/3"></aside>
+        <aside className="w-full lg:w-1/2 2xl:1/3 md:p-11 space-y-11 flex flex-col items-center">
+          <a href="/thomas">
+            <div className="w-full cursor-pointer flex flex-col space-y-3 items-center">
+              <div className="flex flex-col items-center space-y-2 md:flex-row md:space-x-5 sm:ml-8">
+                <img src="./thomas.png" className="" alt="Thomas" />
+                <p className="text-center text-fuchsia-900 w-4/5 text-xs sm:text-start sm:w-[250px]">
+                  Die beste Katzenwebsite, die es in der Slowakei gibt. Ich
+                  hoffe, dass es gut übersetzt ist und nicht irgendein Mist.
+                </p>
+              </div>
+              <div className="w-full flex flex-col items-center sm:flex-row">
+                <div>
+                  <p className="text-fuchsia-500 font-bold ">THOMAS STYKALEI</p>
+                  <p className="text-fuchsia-700 font-bold ">THOMAS STYKALEI</p>
+                  <p className="text-fuchsia-900 font-bold ">THOMAS STYKALEI</p>
+                </div>
+              </div>
+            </div>
+          </a>
+        </aside>
       </div>
     </>
   );

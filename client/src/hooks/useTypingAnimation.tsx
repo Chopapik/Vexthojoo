aa48fa876110b9a7b7e4f75bb9bb94d4ab6bf509
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2024-12-04 20:32:56 +0100

    Create useTypingAnimation hook with typing and cursor animation"

---
+++ client/src/hooks/useTypingAnimation.tsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+
+const useTypingAnimation = (
+  inputContent: string,
+  trigger: boolean,
+  startTypingDelay: number,
+  cursorBlinkingDelay: number
+) => {
+  const [cursor, setCursor] = useState<string>("");
+  const [content, setContent] = useState<string>("");
+  const [isTyping, setIsTyping] = useState(false);
+  //cursor animation
+  useEffect(() => {
+    console.log(inputContent);
+    const interval = setInterval(() => {
+      setCursor((prevCursor) => (prevCursor === "" ? "▌" : ""));
+    }, cursorBlinkingDelay);
+    return () => clearInterval(interval);
+  }, []);
+  //typing animation
+  useEffect(() => {
+    setContent("");
+    setIsTyping(true);
+
+    setTimeout(() => {
+      for (let i = 0; i <= inputContent.length; i++) {
+        setTimeout(() => {
+          setContent((prev) => prev + inputContent.charAt(i));
+          if (i === inputContent.length - 1) {
+            setIsTyping(false);
+          }
+        }, 40 * i);
+      }
+    }, startTypingDelay);
+  }, [trigger, inputContent, startTypingDelay]);
+
+  return { content, cursor, isTyping };
+};
+
+export default useTypingAnimation;

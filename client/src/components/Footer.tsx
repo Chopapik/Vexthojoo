8b00c5d9dc7489776c3526c5bad060fa9e2475a0
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2024-12-06 19:16:01 +0100

    Fix issue in footer that provides high CLS

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,10 +1,26 @@
+import { useState, useEffect } from "react";
+
 const Footer = () => {
+  const [showFooter, setShowFooter] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowFooter(true);
+    }, 10);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="w-full flex justify-center mt-10 font-roboto">
-      <p className="text-xs text-cyan-500">
-        <strong>Chopapik</strong> &copy; 2024
-      </p>
-    </div>
+    <>
+      {showFooter && (
+        <footer className="relative bottom-0 left-0 w-full text-xs py-1 bg-black flex justify-center items-center">
+          <span className="text-cyan-500">
+            <strong>Chopapik</strong> &copy; 2024
+          </span>
+        </footer>
+      )}
+    </>
   );
 };
+
 export default Footer;

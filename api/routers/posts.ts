ac4926312ac1bad6ad17443df1dc9d4fd3c3d445
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2024-12-07 10:07:29 +0100

    Add multer middleware to /addPost and multer postsImages dest

--- api/routers/posts.ts
+++ api/routers/posts.ts
@@ -1,5 +1,10 @@
 import express from "express";
 const router = express.Router();
+import multer from "multer";
+
+const upload = multer({
+  dest: "./uploads/postsImages",
+});
 
 import {
   printAllPosts,
@@ -8,7 +13,7 @@ import {
 } from "../controllers/postsController";
 
 router.get("/printAllPosts", printAllPosts);
-router.post("/addPost", addPost);
+router.post("/addPost", upload.single("image"), addPost);
 router.delete("/removePost/:postid", removePost);
 
 export default router;

7a1a1c95a2f9c13853376d5716d5e0139fbeff32
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2024-12-13 13:01:18 +0100

    Implement handling of register data from input fields

--- client/src/hooks/useRegister.ts
+++ client/src/hooks/useRegister.ts
@@ -16,10 +16,26 @@ const useRegister = () => {
     acceptTerm: false,
   });
 
+  const handleSetUsername = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setRegisterData({ ...registerData, username: e.target.value });
+  };
+
+  const handleSetPassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setRegisterData({ ...registerData, password: e.target.value });
+  };
+
+  const handleSetRePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setRegisterData({ ...registerData, rePassword: e.target.value });
+  };
+
+  const handleSetAcceptTerm = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setRegisterData({ ...registerData, acceptTerm: e.target.checked });
+  };
+
   const [registerError, setRegisterError] = useState<registerErrorTypes>({
-    usernameError: null,
-    passwordError: null,
-    acceptTermError: null,
+    usernameError: undefined,
+    passwordError: undefined,
+    acceptTermError: undefined,
   });
   const handleRegister = async () => {
     const response = await registerService(registerData);
@@ -40,7 +56,16 @@ const useRegister = () => {
     }
   };
 
-  return { handleRegister, registerData, setRegisterData, registerError };
+  return {
+    handleRegister,
+    registerData,
+    setRegisterData,
+    registerError,
+    handleSetUsername,
+    handleSetPassword,
+    handleSetRePassword,
+    handleSetAcceptTerm,
+  };
 };
 
 export default useRegister;

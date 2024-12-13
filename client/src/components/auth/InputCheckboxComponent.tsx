b549c0f5e725e0bd979bf0526c4ae7e849322284
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2024-12-13 13:07:44 +0100

    Create component  for inputs layout with errors handling

---
+++ client/src/components/auth/InputCheckboxComponent.tsx
@@ -0,0 +1,35 @@
+const InputCheckboxComponent = ({
+  handleInputData,
+  label,
+  error,
+  enableErrorMessage,
+}: {
+  handleInputData: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  label: string;
+  error?: string;
+  enableErrorMessage: boolean;
+}) => {
+  return (
+    <div className="text-xs flex flex-col items-center space-y-2">
+      <div className="flex space-x-2">
+        <input
+          type="checkbox"
+          name="acceptTerm"
+          onChange={handleInputData}
+          className="accent-cyan-400"
+        />
+        <label htmlFor="rulesAccept">{label}</label>
+      </div>
+
+      <div>
+        {enableErrorMessage && (
+          <span className="relative flex justify-center text-red-600">
+            {error}
+          </span>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default InputCheckboxComponent;
---
+++ client/src/components/auth/InputTextComponent.tsx
@@ -0,0 +1,38 @@
+import { ChangeEvent } from "react";
+
+const InputTextComponent = ({
+  type,
+  label,
+  error,
+  handleInputData,
+  enableErrorMessage,
+}: {
+  type: string;
+  label: string;
+  error?: string;
+  handleInputData: (e: ChangeEvent<HTMLInputElement>) => void;
+  enableErrorMessage: boolean;
+}) => {
+  return (
+    <div className="relative">
+      <input
+        type={type}
+        className={`input01 border-b-2 ${
+          error ? "border-red-600" : "border-white"
+        }`}
+        placeholder=""
+        onChange={handleInputData}
+      />
+      <label htmlFor="username" className="label01">
+        {label}
+      </label>
+      {enableErrorMessage && (
+        <span className="text-xs text-red-600 absolute right-0 top-10">
+          {error}
+        </span>
+      )}
+    </div>
+  );
+};
+
+export default InputTextComponent;

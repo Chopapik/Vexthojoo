043e4d4ce2e44f3a8df62aab5c3feb5b33b713bf
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2024-12-15 20:44:00 +0100

    Fix login-register/cookie-data bar in head component

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -22,54 +22,57 @@ const Header = () => {
   }, []);
 
   return (
-    <div className="flex flex-col items-center space-y-8 p-4 lg:flex-row lg:justify-between lg:space-y-0">
+    <div className="flex flex-col items-center space-y-8 p-4 lg:flex-row lg:justify-between lg:space-y-0 ">
       <div className="space-y-1">
         <a href="/">
           <img src={logo} alt="VEXTHOJOO" />
         </a>
         <hr className="w-3/4 border-fuchsia-500 border sm:w-[125%]" />
       </div>
-
-      {!loading &&
-        (authData.isLoggedIn ? (
-          <div className="p-4 flex space-x-3">
-            <div className="flex flex-col justify-center">
-              <span
-                id="loggedUserUsername"
-                className="text-neutral-600 text-xs"
-              >
-                o siema
-              </span>
-              <span className="text-white font-bold">{authData.username}</span>
-            </div>
-            <div>
-              <div className="w-11 h-11 border border-neutral-600">
-                <a href={`/${authData.username}`}>
-                  <img
-                    src={authData.avatar ? authData.avatar : defaultAvatar}
-                    alt="Avatar"
-                    className="object-cover w-11 h-11"
-                  />
-                </a>
+      <div className="min-h-[90px]">
+        {!loading &&
+          (authData.isLoggedIn ? (
+            <div className="p-4 flex space-x-3 ">
+              <div className="flex flex-col justify-center">
+                <span
+                  id="loggedUserUsername"
+                  className="text-neutral-600 text-xs"
+                >
+                  o siema
+                </span>
+                <span className="text-white font-bold">
+                  {authData.username}
+                </span>
+              </div>
+              <div>
+                <div className="w-11 h-11 border border-neutral-600">
+                  <a href={`/${authData.username}`}>
+                    <img
+                      src={authData.avatar ? authData.avatar : defaultAvatar}
+                      alt="Avatar"
+                      className="object-cover w-11 h-11"
+                    />
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
-        ) : (
-          <div className="flex flex-col space-y-3 text-white xs:flex-row xs:space-y-0 xs:space-x-4">
-            <button
-              className="button01 bg-fuchsia-500 hover:shadow-button01 hover:shadow-fuchsia-500"
-              onClick={() => showPanel("loginPanel")}
-            >
-              LOGOWANIE
-            </button>
-            <button
-              className="button01 bg-black border-2 border-fuchsia-600 hover:shadow-button01 hover:shadow-fuchsia-500"
-              onClick={() => showPanel("registerPanel")}
-            >
-              REJESTRACJA
-            </button>
-          </div>
-        ))}
+          ) : (
+            <div className="flex flex-col space-y-3 text-white xs:flex-row xs:space-y-0 xs:space-x-4 min-h-ful">
+              <button
+                className="button01 bg-fuchsia-500 hover:shadow-button01 hover:shadow-fuchsia-500"
+                onClick={() => showPanel("loginPanel")}
+              >
+                LOGOWANIE
+              </button>
+              <button
+                className="button01 bg-black border-2 border-fuchsia-600 hover:shadow-button01 hover:shadow-fuchsia-500"
+                onClick={() => showPanel("registerPanel")}
+              >
+                REJESTRACJA
+              </button>
+            </div>
+          ))}
+      </div>
     </div>
   );
 };

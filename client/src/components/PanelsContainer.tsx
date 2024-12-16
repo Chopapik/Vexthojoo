d47e08009c9054ebfc900741facfea66810b974b
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2024-12-16 18:17:44 +0100

    Remove unused imports and props from EditUserPanel

--- client/src/components/PanelsContainer.tsx
+++ client/src/components/PanelsContainer.tsx
@@ -1,32 +1,17 @@
-//The PanelsContaine component contains all the project's panels, which are always positioned above other component
-import EditUserPanel from "./EditUserPanel"; //to refactor
-
-import { PanelContext } from "../context/PanelContext";
-import { useContext } from "react";
+//The PanelsContainer component contains all the project's panels, which are always positioned above other component
 
 import LoginPanel from "./auth/Login/LoginPanel";
 import RegisterPanel from "./auth/Register/RegisterPanel";
 import AddPostPanel from "./posts/addPost/AddPostPanel";
-
+import EditUserPanel from "./user/EditUserPanel";
 const PanelsContainer = () => {
-  const context = useContext(PanelContext);
-
-  if (!context) {
-    return <div className="text-red">CONTEXT ERR</div>;
-  }
-  const { closePanel, visiblePanelId } = context;
-
   return (
     <>
       {/* NEW COMPONENT */}
       <RegisterPanel />
       <LoginPanel />
       <AddPostPanel />
-
-      <EditUserPanel
-        closePanelFunction={closePanel}
-        visiblePanelId={visiblePanelId}
-      />
+      <EditUserPanel />
     </>
   );
 };

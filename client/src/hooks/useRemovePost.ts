bc9076f5c9125ec53132c26694d53d5f1123f94c
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2024-12-18 21:43:40 +0100

    Create useRemove hook with implement post removal  and enable delete mode

---
+++ client/src/hooks/useRemovePost.ts
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { postDataTypes } from "../types/posts/postTypes";
+import removePostService from "../services/posts/removePostService";
+
+const useRemovePost = (
+  postsData: postDataTypes[],
+  handleFetchingPosts: () => void
+) => {
+  const [deleteModeEnable, setDeleteModeEnable] = useState<boolean[]>(
+    new Array(postsData.length).fill(false)
+  );
+
+  const handleDeleteModeEnable = (index: number) => {
+    setDeleteModeEnable((prevState) => {
+      const newState = [...prevState];
+      newState[index] = !newState[index];
+      return newState;
+    });
+  };
+
+  const handleDeletePost = async (id: number, index: number) => {
+    await removePostService(id);
+    console.log();
+    //After post remove, post delete mode will be reset to false
+    setDeleteModeEnable((prevState) => {
+      const newState = [...prevState];
+      newState[index] = !newState[index];
+      return newState;
+    });
+    handleFetchingPosts();
+  };
+
+  return { deleteModeEnable, handleDeleteModeEnable, handleDeletePost };
+};
+
+export default useRemovePost;

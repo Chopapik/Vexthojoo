e1eb32022414027a0101179eb18c0664faa8c74f
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2024-12-19 08:41:33 +0100

    Add id, handleDeleteModeEnable, handleDeletePost to postTypes interface

--- client/src/types/posts/postTypes.ts
+++ client/src/types/posts/postTypes.ts
@@ -1,4 +1,5 @@
 export interface postDataTypes {
+  id: number;
   username: string;
   avatarPath: string;
   whenUpload: string;
@@ -8,10 +9,17 @@ export interface postDataTypes {
 }
 
 export interface postTypes {
+  id: number;
   index: number;
   postData: postDataTypes;
   postOpacity: boolean[];
+  //postOpacity array contains opacity state for each post, which is used to control  the animation effect for displaying
   enableOptions?: boolean;
+  deleteModeEnable?: boolean;
+  handleDeleteModeEnable: (index: number) => void;
+  handleDeletePost: (id: number, index: number) => void;
+  // "id" is the unique identifier in database
+  // "index" is position of the post in the list
 }
 
 export interface postContentDataTypes {

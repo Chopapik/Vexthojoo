79a9e70999c783aa74e686405b05ff630a733d0b
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2024-12-19 09:01:41 +0100

    Add post deletion layout with delete confirmation

--- client/src/components/posts/Post.tsx
+++ client/src/components/posts/Post.tsx
@@ -3,20 +3,33 @@ import { postTypes } from "../../types/posts/postTypes";
 import defaultAvatar from "../../assets/images/defaultAvatar.png";
 import deleteIcon from "../../assets/icons/delete.svg";
 import editIcon from "../../assets/icons/edit.svg";
+import yesIcon from "../../assets/icons/yes.svg";
+import noIcon from "../../assets/icons/no.svg";
 
-const Post = ({ index, postData, postOpacity, enableOptions }: postTypes) => {
+const Post = ({
+  id,
+  index,
+  postData,
+  postOpacity,
+  enableOptions,
+  deleteModeEnable,
+  handleDeleteModeEnable,
+  handleDeletePost,
+}: postTypes) => {
   return (
     postData && (
       <>
         <div
           key={index}
-          className={`w-full bg-neutral-900 p-5 space-y-4 ${
+          className={`w-full bg-neutral-900 p-5 space-y-4 border-l border-b border-neutral-700 ${
+            deleteModeEnable && " border-red-700"
+          } ${
             postOpacity[index] ? "opacity-100" : "opacity-0"
           } transition-all ease-linear duration-200`}
         >
           <div className="flex flex-row justify-between">
-            <div className="flex flex-row space-x-2 ">
-              <div className=" w-14 h-14 border border-neutral-600 ">
+            <div className="flex flex-row space-x-2">
+              <div className="w-14 h-14 border border-neutral-600">
                 <a href={`/${postData.username}`}>
                   <img
                     src={
@@ -38,12 +51,39 @@ const Post = ({ index, postData, postOpacity, enableOptions }: postTypes) => {
               </div>
             </div>
             {enableOptions && (
-              <div className="h-7 flex space-x-2">
-                <div className="h-7 w-7 flex justify-center items-center bg-neutral-700 hover:bg-neutral-500 cursor-pointer transition-all ease-linear duration-150">
+              <div className="flex h-7 space-x-2">
+                {deleteModeEnable && (
+                  <div className="h-7 w-auto px-3 flex justify-center items-center bg-neutral-700">
+                    Czy na pewno usunąć post?
+                    <div className="flex ml-3 space-x-1 w-[50px]">
+                      <div className="flex justify-center items-center ">
+                        <img
+                          src={yesIcon}
+                          alt="yes"
+                          onClick={() => handleDeletePost(id, index)}
+                          className="hover:brightness-50"
+                        />
+                      </div>
+                      <div className="flex justify-center items-center">
+                        <img
+                          src={noIcon}
+                          alt="no"
+                          onClick={() => handleDeleteModeEnable(index)}
+                          className="hover:brightness-50"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                )}
+
+                <div className="h-7 w-7 flex justify-center items-center bg-neutral-700 hover:bg-neutral-500 cursor-pointer">
                   <img src={editIcon} alt="edit post" />
                 </div>
 
-                <div className="h-7 w-7 flex justify-center items-center bg-red-700 hover:bg-red-500 cursor-pointer transition-all ease-linear duration-150">
+                <div
+                  className="h-7 w-7 flex justify-center items-center bg-red-700 hover:bg-red-500 cursor-pointer"
+                  onClick={() => handleDeleteModeEnable(index)}
+                >
                   <img src={deleteIcon} alt="remove post" />
                 </div>
               </div>
@@ -53,8 +93,8 @@ const Post = ({ index, postData, postOpacity, enableOptions }: postTypes) => {
           {postData.imagePath ? (
             <img
               src={postData.imagePath}
-              className="min-w-[25%] max-w-[55%]"
-            ></img>
+              className="min-w-[25%] max-w-[55%] border-l border-b border-neutral-700"
+            />
           ) : null}
         </div>
       </>

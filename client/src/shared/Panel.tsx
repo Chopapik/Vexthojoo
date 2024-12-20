e8c28a7c3b93257ce5fd763be35dec6227dda406
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2024-12-20 23:10:44 +0100

    Fix panel position

--- client/src/shared/Panel.tsx
+++ client/src/shared/Panel.tsx
@@ -13,7 +13,7 @@ const Panel = ({
     <>
       {isVisible && (
         <>
-          <div className="absolute bg-black w-full h-full flex justify-center opacity-80 z-50" />
+          <div className="fixed bg-black w-full h-full flex justify-center opacity-80 z-50" />
           <div className="w-screen h-screen fixed flex justify-center z-50">
             <div className="fixed mt-[5%] pl-[2px] pb-[2px] flex justify-end bg-neutral-700">
               <div className="bg-neutral-800 text-white min-h-[200px] w-[300px] md:min-w-[450px] px-4 pt-4 pb-9">

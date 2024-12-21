50184ffa6c3a34528fdf3c1902867a6d3ed02a2f
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2024-12-21 13:58:52 +0100

    Add CookieAcceptBar to PanelsContainer

--- client/src/components/PanelsContainer.tsx
+++ client/src/components/PanelsContainer.tsx
@@ -1,9 +1,13 @@
-//The PanelsContainer component contains all the project's panels, which are always positioned above other component
+//The PanelsContainer component contains all the project's panels*, which are always positioned above other component
+
+//* or components with fixed display property
 
 import LoginPanel from "./auth/Login/LoginPanel";
 import RegisterPanel from "./auth/Register/RegisterPanel";
 import AddPostPanel from "./posts/addPost/AddPostPanel";
 import EditUserPanel from "./user/EditUserPanel";
+import CookieAcceptBar from "./CookieAcceptBar";
+
 const PanelsContainer = () => {
   return (
     <>
@@ -11,6 +15,7 @@ const PanelsContainer = () => {
       <LoginPanel />
       <AddPostPanel />
       <EditUserPanel />
+      <CookieAcceptBar />
     </>
   );
 };

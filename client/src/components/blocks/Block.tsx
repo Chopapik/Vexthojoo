5be3cc0c371944da3975ae671bd9fdbb4c84b4f3
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2024-12-21 14:02:59 +0100

    Add blocking create of cookie with boxs visibility state if cookies isn't accepted

--- client/src/components/blocks/Block.tsx
+++ client/src/components/blocks/Block.tsx
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import arrowIcon from "../../assets/icons/upArrrow.svg";
+import cookie from "js-cookie";
+import { useCookieAcceptContext } from "../../context/CookieAcceptContext";
 
 const Block = ({
   content,
@@ -10,13 +12,36 @@ const Block = ({
   name: string;
   background: string;
 }) => {
-  const [rotateButtonBlock2, setRotateButtonBlock2] = useState(false);
+  const { isCookieAccept } = useCookieAcceptContext();
+
+  const [hideBlock, setHideBlock] = useState<boolean>(
+    cookie.get(`${name}State`) === "hidden"
+  );
+
+  const handleBlockState = () => {
+    setHideBlock((prev) => {
+      const newState = !prev;
+      if (isCookieAccept)
+        cookie.set(`${name}State`, newState ? "hidden" : "visible");
+
+      return newState;
+    });
+  };
+
+  useEffect(() => {
+    const cookieState = cookie.get(`${name}State`);
+    if (cookieState === "hidden") {
+      setHideBlock(true);
+    } else {
+      setHideBlock(false);
+    }
+  }, []);
 
   return (
     <>
       <div
         className={`w-full py-1 xs:w-3/4 md:w-3/4 lg:w-full xl:w-3/4 ${
-          rotateButtonBlock2 ? "h-8" : "h-[550px]"
+          hideBlock ? "h-8" : "h-[550px]"
         } text-sm ${background} rounded-xl overflow-hidden flex flex-col justify-between transition-all ease-in-out duration-200`}
       >
         <div>
@@ -25,10 +50,10 @@ const Block = ({
             <img
               src={arrowIcon}
               className={`w-5 float-right cursor-pointer transition-transform ease-in duration-200 ${
-                rotateButtonBlock2 ? "rotate-180" : ""
+                hideBlock ? "rotate-180" : ""
               }`}
               alt="Hide/Show"
-              onClick={() => setRotateButtonBlock2((prev) => !prev)}
+              onClick={handleBlockState}
             />
           </div>
         </div>

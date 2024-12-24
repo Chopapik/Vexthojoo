d28a79fa67a3ec524874b9a597b825c54171f421
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2024-12-24 14:01:03 +0100

    Add auto bottom margin for accpet cookie bar

--- client/src/components/PageContainer.tsx
+++ client/src/components/PageContainer.tsx
@@ -1,7 +1,18 @@
 import { ReactNode } from "react";
+import { useCookieAcceptContext } from "../context/CookieAcceptContext";
 
 const PageContainer = ({ children }: { children: ReactNode }) => {
-  return <div className="bg-black min-h-screen">{children}</div>;
+  const { isCookieAccept } = useCookieAcceptContext();
+
+  return (
+    <div
+      className={`bg-black min-h-screen ${
+        !isCookieAccept && "mb-[300px] sm:mb-[100px]"
+      }`}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default PageContainer;

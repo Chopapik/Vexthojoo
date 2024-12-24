c8bff31b131b3f5a40b137530c8d181d2aedb1f9
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2024-12-24 10:03:20 +0100

    Rebuild accept cookie bar

--- client/src/components/CookieAcceptBar.tsx
+++ client/src/components/CookieAcceptBar.tsx
@@ -1,5 +1,6 @@
 import Button01 from "./buttons/Button01";
 import { useCookieAcceptContext } from "../context/CookieAcceptContext";
+import cookieImage from "../assets/images/cookie.png";
 
 const CookieAcceptBar = () => {
   const { handleAcceptCookie, showAcceptCookieBar } = useCookieAcceptContext();
@@ -7,40 +8,35 @@ const CookieAcceptBar = () => {
   if (!showAcceptCookieBar) return null;
 
   return (
-    <>
-      <div className="fixed bg-black w-full h-full opacity-80 z-50"></div>
-      <div className="w-screen h-screen fixed flex flex-col items-center justify-end z-50">
-        <div className="bg-neutral-900 w-4/5 p-5 border border-neutral-600 space-y-8 flex flex-col sm:flex-row sm:space-y-0 sm:p-7 mb-10">
-          <div className="text-white flex flex-col space-y-1 sm:flex">
-            <span className="italic font-bold text-xl text-center mb-4 sm:mb-0 sm:text-left">
-              UŻYTKOWNIKU
-            </span>
-            <span>Ta strona korzysta z ciasteczek</span>
-            <span className="text-xs">
-              Ciasteczka są wykorzystywane do utrzymania sesji logowania oraz
-              przechowywania ustawień stylów i wyglądu strony.
-            </span>
-            <span className="text-xs">Zaleca się akceptację ciasteczek.</span>
-          </div>
-          <div className="w-full flex justify-center">
-            <div className="w-1/2 sm:w-[150px] space-y-2 sm:h-full flex flex-col justify-center items-center">
-              <Button01
-                content={"AKCEPTUJĘ"}
-                color={"bg-cyan-500"}
-                border={"border-green-600"}
-                onClick={() => handleAcceptCookie(true)}
-              />
-              <Button01
-                content={"Ń AKCEPTUJĘ"}
-                color={"bg-cyan-700"}
-                border={"border-red-600"}
-                onClick={() => handleAcceptCookie(false)}
-              />
-            </div>
-          </div>
+    <div className="fixed z-50 bottom-0 w-screen bg-neutral-900 p-5 md:p-1 border-y border-neutral-600 flex sm:space-y-0 justify-center">
+      <div className="flex flex-col space-y-5 p-5 items-center sm:items-start sm:flex-row sm:space-x-8 sm:space-y-0 sm:max-w-[700px] xl:max-w-[700px] text-white">
+        <div className="flex flex-col sm:h-full justify-center items-center">
+          <img
+            src={cookieImage}
+            alt="cookie"
+            className="w-[80px] sm:w-[150px]"
+          />
+        </div>
+        <div className=" font-poppins text-xs text-center sm:text-left space-y-2">
+          <p>
+            Ta strona wykorzystuje pliki cookies do utrzymania sesji logowania
+            oraz zapisywania preferencji dotyczących stylów i wyglądu strony.
+          </p>
+          <p>
+            Klikając przycisk "Akceptuję", zgadzasz się na wykorzystywanie
+            plików cookies.
+          </p>
+        </div>
+        <div className="sm:h-full flex flex-col justify-center items-center ">
+          <Button01
+            content={"Akceptuję"}
+            color={"bg-fuchsia-500"}
+            border={"fuchsia"}
+            onClick={handleAcceptCookie}
+          />
         </div>
       </div>
-    </>
+    </div>
   );
 };
 

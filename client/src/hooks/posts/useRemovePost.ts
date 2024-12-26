847bc71fd6627d75b42ac1132d05908c6629159f
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2024-12-26 20:16:04 +0100

    Replace handleFetchingPosts prop with usePostsContext

--- client/src/hooks/posts/useRemovePost.ts
+++ client/src/hooks/posts/useRemovePost.ts
@@ -1,14 +1,14 @@
 import { useState } from "react";
 import { postDataTypes } from "../../types/posts/postTypes";
 import removePostService from "../../services/posts/removePostService";
+import { usePostsContext } from "../../context/PostsContext";
 
-const useRemovePost = (
-  postsData: postDataTypes[],
-  handleFetchingPosts: () => void
-) => {
+const useRemovePost = (postsData: postDataTypes[]) => {
   const [deleteModeEnable, setDeleteModeEnable] = useState<boolean[]>(
     new Array(postsData.length).fill(false)
   );
+
+  const { handleFetchingPosts } = usePostsContext();
 
   const handleDeleteModeEnable = (index: number) => {
     setDeleteModeEnable((prevState) => {

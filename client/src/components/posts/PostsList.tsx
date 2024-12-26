2c6f781c3c1fb58bb3e189f6ff77cf7153ab8aec
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2024-12-26 20:17:33 +0100

    Add handleFetchingPosts call in useEffect

--- client/src/components/posts/PostsList.tsx
+++ client/src/components/posts/PostsList.tsx
@@ -2,8 +2,9 @@ import PostSkeleton from "./PostSkeleton";
 import Post from "./Post";
 import useListPosts from "../../hooks/posts/useListPosts";
 import useRemovePost from "../../hooks/posts/useRemovePost";
-
 import { postDataTypes } from "../../types/posts/postTypes";
+import { usePostsContext } from "../../context/PostsContext";
+import { useEffect } from "react";
 
 const PostsList = ({
   displayByUser,
@@ -12,11 +13,16 @@ const PostsList = ({
   displayByUser?: string;
   enableOptions: boolean;
 }) => {
-  const { loading, postsData, postOpacity, handleFetchingPosts } =
-    useListPosts(displayByUser);
+  const { loading, postsData, postOpacity } = useListPosts();
 
   const { deleteModeEnable, handleDeletePost, handleDeleteModeEnable } =
-    useRemovePost(postsData, handleFetchingPosts);
+    useRemovePost(postsData);
+
+  const { handleFetchingPosts } = usePostsContext();
+
+  useEffect(() => {
+    handleFetchingPosts(displayByUser);
+  }, [displayByUser]);
 
   return (
     <>

b69b5c5576271427d1ba3494c6eebbc6e9ef66c2
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2024-12-27 21:22:53 +0100

    Implement saving updated posts' text data in database

---
+++ api/controllers/posts/updatePost.ts
@@ -0,0 +1,24 @@
+import db from "../../mysqlConnection";
+import { Request, Response } from "express";
+
+const updatePost = async (req: Request, res: Response) => {
+  const { newText } = req.body;
+  //   const newImage = req.file;
+
+  const postId: number = parseInt(req.params.postId, 10);
+
+  try {
+    if (newText) {
+      await db.query("UPDATE posts SET text = ? WHERE id = ?", [
+        newText,
+        postId,
+      ]);
+      res.end();
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+    console.log(error);
+  }
+};
+
+export default updatePost;
--- api/routers/posts.ts
+++ api/routers/posts.ts
@@ -4,6 +4,7 @@ import multer from "multer";
 import fetchPosts from "../controllers/posts/fetchPosts";
 import addPost from "../controllers/posts/addPost";
 import removePost from "../controllers/posts/removePost";
+import updatePost from "../controllers/posts/updatePost";
 
 const upload = multer({
   dest: "./uploads/postsImages",
@@ -12,5 +13,5 @@ const upload = multer({
 router.get("/printAllPosts", fetchPosts);
 router.post("/addPost", upload.single("image"), addPost);
 router.delete("/removePost/:postid", removePost);
-
+router.put("/updatePost/:postId", upload.single("image"), updatePost);
 export default router;

09a63fc548fd89a103bbc8a1f5d1c81a1db2c927
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2024-12-28 18:35:20 +0100

    Refactor CookieAthContext and import context safety

--- client/src/context/CookieAuthContext.tsx
+++ client/src/context/CookieAuthContext.tsx
@@ -1,21 +1,23 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useContext } from "react";
 import { CookieAuthTypes } from "../types/auth/cookieAuthTypes";
 import useCookieAuth from "../hooks/cookies/useCookieAuth";
 
 interface CookieAuthContext {
   authData: CookieAuthTypes;
-  getUser: () => void;
+  getUser: () => Promise<void>;
 }
+const CookieAuthContext = createContext<CookieAuthContext | undefined>(
+  undefined
+);
 
-export const CookieAuthContext = createContext<CookieAuthContext>({
-  authData: {
-    isLoggedIn: false,
-    userid: null,
-    username: null,
-    avatarPath: null,
-  },
-  getUser: async () => {},
-});
+export const useCookieAuthContext = () => {
+  const context = useContext(CookieAuthContext);
+  if (!context) {
+    throw new Error("CookieAuthContext must be used within CookieAuthProvider");
+  }
+
+  return context;
+};
 
 export const CookieAuthProvider = ({ children }: { children: ReactNode }) => {
   const { getUser, authData } = useCookieAuth();

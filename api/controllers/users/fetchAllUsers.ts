32092b38b57d187d1d2111afa4c3e0d6792c1cbe
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2024-12-31 23:17:08 +0100

    Implement fetching all users' data from database

---
+++ api/controllers/users/fetchAllUsers.ts
@@ -0,0 +1,18 @@
+import db from "../../mysqlConnection";
+import { Request, Response } from "express";
+
+const fetchAllUsers = async (req: Request, res: Response) => {
+  try {
+    const allUsers = await db.query(
+      "SELECT * FROM users ORDER BY username ASC"
+    );
+
+    console.log(allUsers);
+
+    res.status(200).json({ allUsers: allUsers[0] });
+  } catch (error) {
+    res.status(500).json({ message: "Błąd wewnętrzny serwera" });
+  }
+};
+
+export default fetchAllUsers;
--- api/routers/user.ts
+++ api/routers/user.ts
@@ -4,13 +4,15 @@ import multer from "multer";
 import userData from "../controllers/users/userData";
 import updateUserData from "../controllers/users/updateUserData";
 import deleteUserAccount from "../controllers/users/deleteUserAccount";
+import fetchAllUsers from "../controllers/users/fetchAllUsers";
 
 const router = express.Router();
 const upload = multer({
   dest: "./uploads/UsersAvatars",
 });
-
+router.get("/allUsers", fetchAllUsers);
 router.get("/:username", userData);
 router.post("/updateData", upload.single("avatar"), updateUserData);
 router.delete("/removeUser/:userIdToRemove", deleteUserAccount);
+
 export default router;

6944662cc2dab53da2dc210a216e24443074b198
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2024-12-31 15:54:58 +0100

    Add error handling view fir query error in chat

--- client/src/components/blocks/chatAi/ChatAiBlock.tsx
+++ client/src/components/blocks/chatAi/ChatAiBlock.tsx
@@ -8,8 +8,13 @@ import sendIcon from "../../../assets/icons/upArrow2.svg";
 const ChatAiBlock = () => {
   const [inputValue, setInputValue] = useState<string>("");
 
-  const { setUserQuestionInput, chatAiColumn, handleChatResponse, isTyping } =
-    useChatAiReponse();
+  const {
+    setUserQuestionInput,
+    chatAiColumn,
+    handleChatResponse,
+    isTyping,
+    queryError,
+  } = useChatAiReponse();
 
   return (
     <>
@@ -21,27 +26,38 @@ const ChatAiBlock = () => {
             <div className="min-h-full flex flex-col justify-between ">
               <div className="w-full flex-grow ">
                 <div className="w-full flex flex-col ">
-                  <div className="flex w-full flex-col text-neutral-100 font-roboto space-y-5 px-3 mt-4 ">
-                    <div className="w-full flex justify-end ">
-                      <div
-                        className={`bg-zinc-700 py-2 px-4 rounded-3xl ${
-                          chatAiColumn.question === "" ? "hidden" : "visible"
-                        } `}
-                      >
-                        <span>{chatAiColumn.question}</span>
+                  {queryError ? (
+                    <>
+                      <div className="w-full h-full mt-14 flex flex-col justify-center items-center font-roboto font-bold italic">
+                        <h2 className="text-xl text-red-600">
+                          Błąd {queryError.status} XD
+                        </h2>
+                        <p className="text-white">{queryError.message}</p>
                       </div>
-                    </div>
-                    <div className="flex space-x-3 overflow-hidden">
-                      <div className="w-11">
-                        <div className="w-10 h-10 border-[2px] border-zinc-700 rounded-full overflow-hidden">
-                          <img src={chatIcon} alt="ico" />
+                    </>
+                  ) : (
+                    <div className="flex w-full flex-col text-neutral-100 font-roboto space-y-5 px-3 mt-4 ">
+                      <div className="w-full flex justify-end ">
+                        <div
+                          className={`bg-zinc-700 py-2 px-4 rounded-3xl ${
+                            chatAiColumn.question === "" ? "hidden" : "visible"
+                          } `}
+                        >
+                          <span>{chatAiColumn.question}</span>
                         </div>
                       </div>
-                      <div className="flex-grow">
-                        <span>{chatAiColumn.response}</span>
+                      <div className="flex space-x-3 overflow-hidden">
+                        <div className="w-11">
+                          <div className="w-10 h-10 border-[2px] border-zinc-700 rounded-full overflow-hidden">
+                            <img src={chatIcon} alt="ico" />
+                          </div>
+                        </div>
+                        <div className="flex-grow">
+                          <span>{chatAiColumn.response}</span>
+                        </div>
                       </div>
                     </div>
-                  </div>
+                  )}
                 </div>
               </div>
               <div className="w-full h-10 mb-5">

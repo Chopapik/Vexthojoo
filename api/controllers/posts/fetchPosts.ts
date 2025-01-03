dbbb1ccdcd062bca05905f2eb735c94c59b164f8
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2025-01-03 02:24:06 +0100

    Fix fetching posts

--- api/controllers/posts/fetchPosts.ts
+++ api/controllers/posts/fetchPosts.ts
@@ -8,13 +8,11 @@ const fetchPosts = async (req: Request, res: Response) => {
       "SELECT p.id,u.username,u.avatarPath,p.text,p.whenUpload,p.whatDevice,p.imagePath,p.isEdited FROM users u JOIN posts p ON p.user_id=u.id ORDER BY p.whenUpload DESC"
     );
 
-    if (posts.length === 0) {
-      res.status(204).json({ message: "Nie znaleziono postów" });
-    }
-
     res.status(200).json(posts);
+    res.end();
   } catch (error) {
     res.status(500).json({ message: "Błąd serwera" });
+    res.end();
     console.log(error);
   }
 };

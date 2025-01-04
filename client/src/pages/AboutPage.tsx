f615bc201f86a2cdfebc21fb2c34fae2c8148726
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2025-01-04 23:48:32 +0100

    Rebuild about page

--- client/src/pages/AboutPage.tsx
+++ client/src/pages/AboutPage.tsx
@@ -1,5 +1,5 @@
 import version_1_0_0_image from "../assets/versionImages/1.0.0.png";
-import version_1_0_1_image from "../assets/versionImages/1.0.1.png";
+import version_1_1_0_image from "../assets/versionImages/1.1.0.png";
 
 import { useState } from "react";
 
@@ -13,20 +13,23 @@ const AboutPage = () => {
 
   const versions: versionType[] = [
     {
-      version: "1.0.1",
-      image: version_1_0_1_image,
-      description: ["wprowadzono zmiany w wyglądzie"],
-      date: "03.01.2025",
+      version: "1.1.0",
+      image: version_1_1_0_image,
+      description: [
+        "wprowadzono niewielkie poprawki w interfejsie",
+        "zaktualizowano zabezpieczenia",
+      ],
+      date: "05.01.2025",
     },
     {
       version: "1.0.0",
       image: version_1_0_0_image,
-      description: ["tego typu"],
+      description: ["tego typu", "pierwsza iteracja wersji beta aplikacji"],
       date: "31.12.2024",
     },
   ];
 
-  const [version, setVersion] = useState<string>("1.0.1");
+  const [version, setVersion] = useState<string>("1.1.0");
 
   // selectedVersion returns the index of the selected version
   const selectedVersion = () => {
@@ -34,51 +37,49 @@ const AboutPage = () => {
   };
 
   return (
-    <div className="text-white w-full flex flex-col flex-1 items-center font-roboto">
-      <div className="flex flex-col items-center w-full space-y-[200px]">
-        <div className="space-y-10 mt-[200px]">
-          {/* version div */}
-          <div>
-            <p className="text-4xl font-bold">
-              VEXTHOJOO W. {versions[selectedVersion()].version}
-            </p>
-            <p className="text-2xl text-neutral-500 font-poppins">
-              {versions[selectedVersion()].date}
-            </p>
-          </div>
-          {/* description */}
-          <div>
-            <span className="font-bold text-md">Co nowego?</span>
-            <ul className="space-y-2 list-disc text-sm pl-6">
-              {versions[selectedVersion()].description.map(
-                (description, descriptionIndex) => (
-                  <li key={descriptionIndex}>{description}</li>
-                )
-              )}
-            </ul>
-          </div>
-          <div className="w-full flex justify-center">
-            <img src={versions[selectedVersion()].image} alt=":?!" />
-          </div>
+    <div className="flex-1 flex flex-col items-center mt-15 text-white p-10 space-y-14">
+      <div className="space-y-5">
+        {/* version div */}
+        <div>
+          <p className="text-4xl font-bold">
+            VEXTHOJOO β {versions[selectedVersion()].version}
+          </p>
+          <p className="text-2xl text-neutral-500 font-poppins">
+            {versions[selectedVersion()].date}
+          </p>
         </div>
 
-        <div className="flex flex-col items-center space-y-5">
-          <span className="font-bold text-md">popzednie wersje:</span>
-          <select
-            onChange={(e) => setVersion(e.target.value)}
-            className="w-[200px] outline-none bg-transparent border-b-2 border-white text-white text-center"
-          >
-            {versions.map((version, key) => (
-              <option
-                key={key}
-                value={version.version}
-                className="bg-transparent text-black"
-              >
-                {version.version}
-              </option>
-            ))}
-          </select>
+        {/* description */}
+        <div className="ml-5">
+          <span className="font-bold text-md">Co nowego?</span>
+          <ul className="space-y-2 list-disc text-sm pl-6">
+            {versions[selectedVersion()].description.map(
+              (description, descriptionIndex) => (
+                <li key={descriptionIndex}>{description}</li>
+              )
+            )}
+          </ul>
         </div>
+        <div className="w-full flex justify-center">
+          <img src={versions[selectedVersion()].image} alt=":?!" />
+        </div>
+      </div>
+      <div className="flex flex-col items-center space-y-5">
+        <span className="font-bold text-md">popzednie wersje:</span>
+        <select
+          onChange={(e) => setVersion(e.target.value)}
+          className="w-[200px] outline-none bg-transparent border-b-2 border-white text-white text-center"
+        >
+          {versions.map((version, key) => (
+            <option
+              key={key}
+              value={version.version}
+              className="bg-transparent text-black"
+            >
+              {version.version}
+            </option>
+          ))}
+        </select>
       </div>
     </div>
   );

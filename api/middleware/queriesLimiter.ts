636c988e02a4c3e4c87dcc41431a08f4122e9ffe
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2025-01-04 23:49:27 +0100

    Implement spam prevention by blocking users by generated key

--- api/middleware/queriesLimiter.ts
+++ api/middleware/queriesLimiter.ts
@@ -1,7 +1,13 @@
-const rateLimit = require("express-rate-limit");
+import rateLimit from "express-rate-limit";
+import { Request } from "express";
 
 export const queriesLimiter = rateLimit({
   windowMs: 60 * 2000, //block user's queries for minute
   max: 50, //max 30 queries per minute
   message: "Przekroczyłeś limit zapytań, spróbuj ponownie później.",
+  keyGenerator: (req: Request) => {
+    const ipv4 = req?.ip?.split(":").pop();
+    const key = ipv4?.replace(/\./g, "");
+    return key || "0";
+  },
 });

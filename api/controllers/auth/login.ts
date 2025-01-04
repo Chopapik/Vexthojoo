f37f42731c847583a5eddb4b616f30f7b46ee687
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2025-01-04 21:09:21 +0100

    Fix validation for login if username is empty

--- api/controllers/auth/login.ts
+++ api/controllers/auth/login.ts
@@ -6,71 +6,74 @@ import jwt from "jsonwebtoken";
 const login = async (req: Request, res: Response) => {
   const { username, password, noLogout } = req.body;
 
-  if (username === null || username === undefined || username === "") {
-    res.status(409).json({ message: "Nie podano nazwy" });
-  }
+  console.log(username);
 
   try {
-    const [findUser] = await db.query("SELECT * FROM users WHERE username=?", [
-      username,
-    ]);
-    // "FindUser" returns an array of objects. In this case there is one object, with user's data.
-    // The query "WHERE username = loginData.username" should return only one object.
-    if (findUser.length > 0) {
-      const [
-        {
-          username: findUser_username,
-          password: findUser_password,
-          id: finUser_userid,
-          avatarPath: findUser_avatarPath,
-        },
-      ] = findUser;
-
-      const isPasswordCorrect = await bcrypt.compare(
-        password,
-        findUser_password
+    if (!username) {
+      res.status(409).json({ message: "Nie podano nazwy" });
+    } else {
+      const [findUser] = await db.query(
+        "SELECT * FROM users WHERE username=?",
+        [username]
       );
+      // "FindUser" returns an array of objects. In this case there is one object, with user's data.
+      // The query "WHERE username = loginData.username" should return only one object.
+      if (findUser.length > 0) {
+        const [
+          {
+            username: findUser_username,
+            password: findUser_password,
+            id: finUser_userid,
+            avatarPath: findUser_avatarPath,
+          },
+        ] = findUser;
 
-      if (isPasswordCorrect) {
-        const secret = process.env.SECRET;
+        const isPasswordCorrect = await bcrypt.compare(
+          password,
+          findUser_password
+        );
 
-        if (secret) {
-          const token = jwt.sign(
-            {
-              username: findUser_username,
-              userid: finUser_userid,
-              avatarPath: findUser_avatarPath,
-            },
-            secret
-          );
+        if (isPasswordCorrect) {
+          const secret = process.env.SECRET;
 
-          const whenLogged = new Date();
+          if (secret) {
+            const token = jwt.sign(
+              {
+                username: findUser_username,
+                userid: finUser_userid,
+                avatarPath: findUser_avatarPath,
+              },
+              secret
+            );
 
-          await db.query(
-            "UPDATE users SET whenLastLogged = ? WHERE username=?",
-            [whenLogged, username]
-          );
+            const whenLogged = new Date();
 
-          if (noLogout) {
-            res.cookie("token", token, {
-              httpOnly: true,
-              maxAge: 10000 * 60 * 60 * 24 * 1024,
-            });
-          } else
-            res.cookie("token", token, {
-              httpOnly: true,
-            });
+            await db.query(
+              "UPDATE users SET whenLastLogged = ? WHERE username=?",
+              [whenLogged, username]
+            );
+
+            if (noLogout) {
+              res.cookie("token", token, {
+                httpOnly: true,
+                maxAge: 10000 * 60 * 60 * 24 * 1024,
+              });
+            } else
+              res.cookie("token", token, {
+                httpOnly: true,
+              });
+          } else {
+            console.log("env secret err");
+          }
+
+          res.send(`${username} logged in`);
+          console.log(`${username} logged in`);
         } else {
-          console.log("env secret err");
+          res.status(409).json({ message: "Zła nazwa lub hasło" });
         }
-
-        res.send(`${username} logged in`);
-        console.log(`${username} logged in`);
       } else {
         res.status(409).json({ message: "Zła nazwa lub hasło" });
       }
-    } else {
-      res.status(409).json({ message: "Zła nazwa lub hasło" });
     }
   } catch (error) {
     res.status(500).json({ message: "Błąd serwera" });

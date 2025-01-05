f2c14c2a195e804675da96c39061c266db47983b
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2025-01-05 23:25:14 +0100

    Add redirect button to 'whats new' page
    Rebuild footer layout

--- client/src/components/layout/Footer.tsx
+++ client/src/components/layout/Footer.tsx
@@ -17,26 +17,29 @@ const Footer = () => {
   return (
     <>
       {showFooter && (
-        <footer className="flex flex-col relative bottom-0 left-0 w-full mb-6 text-xs bg-black items-center h-[8rem] justify-end font-poppins">
-          <div className="w-3/4 flex flex-col items-center space-y-2">
+        <footer className="flex flex-col absolute bottom-0 left-0 w-full mb-6 text-xs bg-black items-center font-poppins">
+          <div className="w-3/4 flex flex-col items-center">
+            <div className="space-x-4 flex items-end justify-center space-y-2 text-neutral-400">
+              <a href="/coNowego" className=" hover:brightness-50">
+                <div className="flex">
+                  <span>co nowego</span>
+                  <div className="w-1.5 h-1.5 rounded-full bg-green-500" />
+                </div>
+              </a>
+              <a href="/oStronie" className="hover:brightness-50">
+                o stronie
+              </a>
+              <a href="/listaUzytkownikow" className="hover:brightness-50">
+                lista użytkowników
+              </a>
+            </div>
             <div className="w-full flex">
               <hr className="border-1 border-neutral-700 my-2 flex-1" />
             </div>
-            <div className="w-full flex justify-between px-1">
-              <div className="text-customCyan-default w-[100px] md:flex-1 ">
+            <div className="w-full flex px-1  justify-center">
+              <div className="text-customCyan-default">
                 <strong>Chopapik</strong>
                 <span> &copy; 2024 - {currentYear}</span>
-              </div>
-              <div className="space-x-4 flex flex-col items-end justify-center space-y-2 md:space-y-0 md:flex-row">
-                <a href="/oStronie" className="text-white hover:brightness-50">
-                  o stronie
-                </a>
-                <a
-                  href="/listaUzytkownikow"
-                  className="text-white hover:brightness-50"
-                >
-                  lista użytkowników
-                </a>
               </div>
             </div>
           </div>
@@ -47,3 +50,5 @@ const Footer = () => {
 };
 
 export default Footer;
+
+// <div className="space-x-4 flex flex-col items-end justify-center space-y-2 md:space-y-0 md:flex-row">

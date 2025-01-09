3ed5446add3c19286633ba91ccf6c33b9efdd3c5
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2025-01-09 04:26:18 +0100

    Change logo size, font sizes and avatar border color

--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -24,19 +24,19 @@ const Header = () => {
     <div className="sticky top-0 bg-black flex flex-col items-center p-2 space-y-4 md:flex-row md:space-y-0 md:justify-between z-49">
       <a href="/">
         <div className="w-fit px-2 space-y-1">
-          <img src={logo} alt="VEXTHOJOO" className="w-[250px]" />
+          <img src={logo} alt="VEXTHOJOO" className="w-[270px]" />
           <hr className="w-3/4 border-fuchsia-500 border md:w-[125%]" />
         </div>
       </a>
       {!loading &&
         (authData.isLoggedIn ? (
           <a href={`/${authData.username}`}>
-            <div className="w-fit flex space-x-5">
-              <div className="flex flex-col text-xs">
-                <span className="text-neutral-600">o siema</span>
-                <span className="font-bold">{authData.username}</span>
+            <div className="w-fit flex space-x-3">
+              <div className="flex flex-col">
+                <span className="text-neutral-600 text-[0.6rem]">o siema</span>
+                <span className="font-bold  text-xs">{authData.username}</span>
               </div>
-              <div className="w-9 h-9 border border-neutral-600">
+              <div className="w-9 h-9 border border-neutral-800">
                 <img
                   src={
                     authData.avatarPath ? authData.avatarPath : defaultAvatar
@@ -67,27 +67,5 @@ const Header = () => {
     </div>
   );
 };
-
-//  <div className="bg-red-900 w-full flex">
-//           <a href={`/${authData.username}`}>
-//             <div className="flex flex-col">
-//               <span className="text-neutral-600 text-xs">o siema</span>
-//               <span className="text-white font-bold">
-//                 {authData.username}
-//                 authData.username
-//               </span>
-//             </div>
-
-//             <div className="w-11 h-11 border border-neutral-600">
-// <img
-//   src={
-//     authData.avatarPath ? authData.avatarPath : defaultAvatar
-//   }
-//   alt="Avatar"
-//   className="object-cover w-full h-full"
-// />
-//             </div>
-//           </a>
-//         </div>
 
 export default Header;

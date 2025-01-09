ac4164aa8bcb0afec1a625d9a7ebe942404363d0
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2025-01-09 04:24:18 +0100

    Remove absoulute and margin classes

--- client/src/components/layout/Footer.tsx
+++ client/src/components/layout/Footer.tsx
@@ -17,9 +17,9 @@ const Footer = () => {
   return (
     <>
       {showFooter && (
-        <footer className="flex flex-col bottom-0 left-0 w-full mb-6 text-xs sm:absolute bg-black items-center font-poppins">
-          <div className="w-3/4 flex flex-col items-center">
-            <div className="space-x-4 flex items-end justify-center space-y-2 text-neutral-400">
+        <footer className="flex flex-col bg-black items-center font-poppins text-center">
+          <div className="w-full flex flex-col items-center">
+            <div className="space-x-4 flex items-end justify-center space-y-2 text-neutral-400 text-[0.7rem]">
               <a href="/coNowego" className=" hover:brightness-50">
                 <div className="flex">
                   <span>co nowego</span>
@@ -37,7 +37,7 @@ const Footer = () => {
               <hr className="border-1 border-neutral-700 my-2 flex-1" />
             </div>
             <div className="w-full flex px-1  justify-center">
-              <div className="text-customCyan-default">
+              <div className="text-customCyan-default text-[0.6rem]">
                 <strong>Chopapik</strong>
                 <span> &copy; 2024 - {currentYear}</span>
               </div>

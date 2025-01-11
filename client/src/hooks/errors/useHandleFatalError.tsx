73abc218f72afa0fb05e302f804f1d2eda5ab093
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2025-01-11 16:08:31 +0100

    Create useHAndleFatalError hook

---
+++ client/src/hooks/errors/useHandleFatalError.tsx
@@ -0,0 +1,41 @@
+import { ErrorType } from "../../types/ErrorType";
+import { usePanelContext } from "../../context/PanelContext";
+import { useState } from "react";
+import errIco from "../../assets/images/err_ico.png";
+
+const useHandleFatalError = () => {
+  const { showPanel } = usePanelContext();
+
+  const [fatalError, setFatalError] = useState<ErrorType | undefined>(
+    undefined
+  );
+
+  const handleFatalErrorData = (fatalError: ErrorType | undefined) => {
+    if (fatalError) {
+      setFatalError(fatalError);
+
+      showPanel(
+        "FatalErrorPanel",
+        <div className="w-[240px] xs:w-[350px] md:w-[400px]  flex flex-col font-roboto justify-between space-y-[70px] overflow-hidden">
+          <div className="w-full flex flex-col items-center space-y-3 text-xl md:text-sm overflow-hidden">
+            <img src={errIco} alt="error" />
+            <span className="font-bold text-red-600">
+              BŁĄD {fatalError.status}
+            </span>
+            <span className="text-center">
+              Wystąpił błąd podczas wysłania zapytania
+            </span>
+          </div>
+          <div className="flex flex-col w-full h-fit justify-end text-neutral-600 text-sm md:text-xs">
+            <span className="font-bold">Szczegóły:</span>
+            <span>{fatalError.message}</span>
+          </div>
+        </div>
+      );
+    }
+  };
+
+  return { handleFatalErrorData, fatalError };
+};
+
+export default useHandleFatalError;

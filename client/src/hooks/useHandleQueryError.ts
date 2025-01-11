831207f1e6a11ede6e58f1e18f72c8d0498f7dd2
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2025-01-11 16:03:13 +0100

    Add if expception if error status will be 500

--- client/src/hooks/useHandleQueryError.ts
+++ client/src/hooks/useHandleQueryError.ts
@@ -1,25 +1,36 @@
 import { useState } from "react";
 import { ErrorType } from "../types/ErrorType";
+import useHandleFatalError from "./errors/useHandleFatalError";
 
 const useHandleQueryError = () => {
   const [queryError, setQueryError] = useState<ErrorType | undefined>(
     undefined
   );
 
+  const { handleFatalErrorData } = useHandleFatalError();
+
   const handleQueryError = (error: ErrorType) => {
-    if (error) {
-      setQueryError({
-        status: error.status,
+    if (error.status === 500) {
+      handleFatalErrorData({
         message: error.message,
-        field: error.field,
+        status: error.status,
       });
     } else {
-      setQueryError({
-        status: undefined,
-        message: "Brak połączenia z serwerem",
-      });
+      if (error) {
+        setQueryError({
+          status: error.status,
+          message: error.message,
+          field: error.field,
+        });
+      } else {
+        setQueryError({
+          status: 500,
+          message: "Brak odpowiedzi serwera",
+        });
+      }
     }
   };
+
   return { handleQueryError, queryError };
 };
 

10286e15a5ed3601cedc65a9372ffebc850518ff
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2025-01-13 14:50:49 +0100

    Add border on top of add post bar

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -18,7 +18,7 @@ const Home = () => {
         <main className="w-full lg:w-2/3 flex flex-col items-center">
           <div className="md:p-3 py-4 space-y-3 w-full sm:w-[640px] md:max-w-none flex flex-col items-center">
             <div
-              className=" bg-neutral-900 px-5 border-l-8 w-3/4 border-neutral-800 py-3 font-poppins cursor-pointer hover:border-customCyan-light box-border transition-colors ease-in-out duration-300"
+              className="bg-neutral-900 px-5 border-l-8 border-t border-neutral-800 border-t-neutral-800 w-3/4 py-3 font-poppins cursor-pointer hover:border-l-customCyan-light box-border transition-colors ease-in-out duration-300"
               onClick={() => {
                 showPanel("addPostPanel");
               }}
@@ -37,7 +37,7 @@ const Home = () => {
             <PostsList enableOptions={false} />
           </div>
         </main>
-        <aside className="w-full lg:w-1/3 max-h-[75vh] lg:sticky lg:top-[70px] flex flex-col items-center px-5 space-y-10 max-w-[400px] ">
+        <aside className="w-full lg:w-1/3 max-h-[75vh] lg:sticky lg:top-[60px] flex flex-col items-center px-5 space-y-10 max-w-[400px] ">
           <hr className="w-full lg:w-0 border-neutral-700" />
           <ChatAiBlock />
           <hr className="w-full border-neutral-700 " />

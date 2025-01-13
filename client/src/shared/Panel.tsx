0dda3de6466f9b79edc695dd2358028a4705215a
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2025-01-13 09:33:22 +0100

    Fix panel's z position

--- client/src/shared/Panel.tsx
+++ client/src/shared/Panel.tsx
@@ -14,8 +14,8 @@ const Panel = ({
     <>
       {isVisible && (
         <>
-          <div className="fixed bg-black w-full h-full flex justify-center opacity-100 z-50" />
-          <div className="w-screen h-screen fixed flex justify-center z-50">
+          <div className="fixed bg-black/80 backdrop-blur-sm w-full h-full flex justify-center z-[998]" />
+          <div className="w-screen h-screen fixed flex justify-center z-[999]">
             <div className="fixed mt-[4vh] border-l-2 border-b-2 border-neutral-800 bg-neutral-900 text-white min-h-[200px] md:min-w-[350px] px-5 py-10">
               <div className="absolute flex justify-end bg-neutral-600 top-1 right-1 transition-all ease-in duration-2">
                 <button

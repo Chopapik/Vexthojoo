4de0f3c56c1e9814a9eaa2de39e0c80d816ac2fe
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2025-01-15 07:31:02 +0100

    Fix skeletons layouts

--- client/src/components/posts/postComponents/PostSkeleton.tsx
+++ client/src/components/posts/postComponents/PostSkeleton.tsx
@@ -3,17 +3,17 @@ const PostSkeleton = () => {
     <>
       <div className="w-full bg-neutral-900 p-5 space-y-4">
         <div className="flex flex-row space-x-2">
-          <div className=" w-14 h-14 border border-neutral-950 bg-neutral-950" />
+          <div className=" w-10 h-10 border border-neutral-950 bg-neutral-950" />
           <div className="space-y-1">
-            <div className="w-[120px] h-3 bg-neutral-950"></div>
-            <div className="w-[70px] h-3 bg-neutral-950"></div>
-            <div className="w-[70px] h-3 bg-neutral-950"></div>
+            <div className="w-[120px] h-2 bg-neutral-950"></div>
+            <div className="w-[70px] h-2 bg-neutral-950"></div>
+            <div className="w-[70px] h-2 bg-neutral-950"></div>
           </div>
         </div>
         <div className="space-y-1">
-          <div className="w-1/4 h-3 bg-neutral-950"></div>
-          <div className="w-1/3 h-3 bg-neutral-950"></div>
-          <div className="w-1/5 h-3 bg-neutral-950"></div>
+          <div className="w-1/4 h-2 bg-neutral-950"></div>
+          <div className="w-1/3 h-2 bg-neutral-950"></div>
+          <div className="w-1/5 h-2 bg-neutral-950"></div>
         </div>
       </div>
     </>
--- client/src/components/user/UserProfileBarSkeleton.tsx
+++ client/src/components/user/UserProfileBarSkeleton.tsx
@@ -1,9 +1,11 @@
 const UserProfileBarSkeleton = () => {
   return (
     <>
-      <div className="flex flex-col items-center space-y-3">
-        <div className="h-8 w-[100px] bg-neutral-950"> </div>
-        <div className="w-32 h-32 bg-neutral-950" />
+      <div className="lg:sticky top-[60px] bg-neutral-900 h-[450px] customBorderNeutral lg:h-[93vh] flex flex-col py-3">
+        <div className="flex flex-col items-center space-y-7">
+          <div className="h-6 w-[120px] bg-neutral-950"> </div>
+          <div className="w-24 h-24 bg-neutral-950"></div>
+        </div>
       </div>
     </>
   );

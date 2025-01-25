5a98e1c9c50c7c4f0c90ab1b10a00b8ef33e9cb4
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2025-01-25 18:19:56 +0100

    Update button01 style and layout

--- client/src/components/buttons/Button01.tsx
+++ client/src/components/buttons/Button01.tsx
@@ -1,56 +1,42 @@
 import { buttonTypes } from "../../types/layout/buttonTypes";
+import buttonStyle from "./Button01.module.css";
 
-const Button01 = ({
+export const ButtonFuchsiaSm = ({
   disableButton,
-  color,
-  shadowColor,
-  border,
   content,
   onClick,
 }: buttonTypes) => {
-  const buttonClass = !disableButton
-    ? color === "red"
-      ? "bg-red-600"
-      : color === "gray"
-      ? "bg-gray-600"
-      : color === "fuchsia"
-      ? "bg-fuchsia-500"
-      : color === "cyan"
-      ? "bg-cyan-500"
-      : color === "black"
-      ? "bg-black"
-      : ""
-    : "hover:shadow-[0_0_0] bg-gray-600";
-
-  const shadowClass = !disableButton
-    ? shadowColor === "red"
-      ? "hover:shadow-red"
-      : shadowColor === "gray"
-      ? "hover:shadow-gray"
-      : shadowColor === "fuchsia"
-      ? "hover:shadow-fuchsia"
-      : shadowColor === "cyan"
-      ? "hover:shadow-cyan"
-      : ""
-    : "";
-
-  return (
+  return disableButton ? (
+    <div className={`${buttonStyle.buttonSm} ${buttonStyle.ButtonSmBlocked}`}>
+      {content}
+    </div>
+  ) : (
     <button
-      className={`${buttonClass} ${border} ${shadowClass} flex justify-center items-center relative z-10 italic 
-      min-w-[110px] min-h-[30px] 
-                    md:min-h-[25px] 
-                   2xl:min-h-[30px] 
-      text-xs font-bold rounded-3xl font-poppins transition-all duration-200 
-        ${
-          disableButton
-            ? "hover:brightness-100 cursor-not-allowed"
-            : "hover:brightness-125 cursor-pointer"
-        }`}
-      onClick={!disableButton ? onClick : undefined}
+      className={`${buttonStyle.buttonSm} bg-fuchsia-500`}
+      onClick={onClick}
     >
       {content}
     </button>
   );
 };
 
-export default Button01;
+export const ButtonFuchsiaSmEmpty = ({
+  disableButton,
+  content,
+  onClick,
+}: buttonTypes) => {
+  return disableButton ? (
+    <div className={`${buttonStyle.buttonSm} ${buttonStyle.ButtonSmBlocked}`}>
+      <div>{content}</div>
+    </div>
+  ) : (
+    <button
+      className={`${buttonStyle.buttonSm} bg-fuchsia-500`}
+      onClick={onClick}
+    >
+      <div className="bg-black w-[126px] h-[26px] rounded-full flex justify-center items-center mb-[1px]">
+        {content}
+      </div>
+    </button>
+  );
+};

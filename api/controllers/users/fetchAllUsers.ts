fcf398289305d9bd15abe4c85fccd1d90d930809
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2025-01-26 17:03:47 +0100

    Add posts counter to SQL query

--- api/controllers/users/fetchAllUsers.ts
+++ api/controllers/users/fetchAllUsers.ts
@@ -4,15 +4,15 @@ import { Request, Response } from "express";
 const fetchAllUsers = async (req: Request, res: Response) => {
   try {
     const allUsers = await db.query(
-      "SELECT * FROM users ORDER BY username ASC"
+      "SELECT u.username, u.avatarPath, u.whenRegist, u.whenLastLogged, p.numberOfPostByUser FROM users u JOIN (SELECT user_id, COUNT(p.id) AS numberOfPostByUser FROM posts p GROUP BY user_id) p ON u.id = p.user_id ORDER BY u.username ASC"
     );
 
     console.log(allUsers);
-
     res.status(200).json({ allUsers: allUsers[0] });
   } catch (error) {
     const errorMessage = (error as Error).message;
     res.status(500).json({ message: errorMessage });
+    console.log(errorMessage);
   }
 };
 

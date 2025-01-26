ad41ee53871f1f48f451acd72bb75bd9b31e78b1
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2025-01-26 11:01:56 +0100

    Update PostSkeleton layout

--- client/src/components/posts/postComponents/PostSkeleton.tsx
+++ client/src/components/posts/postComponents/PostSkeleton.tsx
@@ -1,19 +1,19 @@
 const PostSkeleton = () => {
   return (
     <>
-      <div className="w-full bg-neutral-900 p-5 space-y-4">
+      <div className="w-full bg-neutral-900 p-5 space-y-4 rounded-xl border-t border-neutral-700">
         <div className="flex flex-row space-x-2">
-          <div className=" w-10 h-10 border border-neutral-950 bg-neutral-950" />
+          <div className=" w-[50px] h-[50px] bg-neutral-950 rounded-xl" />
           <div className="space-y-1">
-            <div className="w-[120px] h-2 bg-neutral-950"></div>
-            <div className="w-[70px] h-2 bg-neutral-950"></div>
-            <div className="w-[70px] h-2 bg-neutral-950"></div>
+            <div className="w-[120px] h-4 bg-neutral-950"></div>
+            <div className="w-[70px] h-3 bg-neutral-950"></div>
+            <div className="w-[70px] h-3 bg-neutral-950"></div>
           </div>
         </div>
         <div className="space-y-1">
-          <div className="w-1/4 h-2 bg-neutral-950"></div>
-          <div className="w-1/3 h-2 bg-neutral-950"></div>
-          <div className="w-1/5 h-2 bg-neutral-950"></div>
+          <div className="w-1/4 h-4 bg-neutral-950"></div>
+          <div className="w-1/3 h-4 bg-neutral-950"></div>
+          <div className="w-1/5 h-4 bg-neutral-950"></div>
         </div>
       </div>
     </>

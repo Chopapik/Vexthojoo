2f30af57ca93f764b394ecf58e372f216b96d11a
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2025-01-31 16:04:00 +0100

    Update User profile bar skeleton layout

--- client/src/components/user/UserProfileBarSkeleton.tsx
+++ client/src/components/user/UserProfileBarSkeleton.tsx
@@ -1,10 +1,10 @@
 const UserProfileBarSkeleton = () => {
   return (
     <>
-      <div className="lg:sticky top-[60px] bg-neutral-900 h-[450px] customBorderNeutral lg:h-[93vh] flex flex-col py-3">
-        <div className="flex flex-col items-center space-y-7">
-          <div className="h-6 w-[120px] bg-neutral-950"> </div>
-          <div className="w-24 h-24 bg-neutral-950"></div>
+      <div className="lg:sticky lg:h-[calc(100vh-70px)]  bg-neutral-900 customBorderNeutral rounded-xl min-h-[365px] max-w-[500px] space-y-4 py-8 flex flex-col items-center">
+        <div className="w-[200px] flex flex-col items-center space-y-4">
+          <div className="w-[125px] h-[125px] rounded-xl bg-neutral-950"></div>
+          <div className="w-[200px] h-6 bg-neutral-950"></div>
         </div>
       </div>
     </>

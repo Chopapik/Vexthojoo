75b586376e5db3d866149607fee5a8a8ae93125a
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2025-02-20 18:52:42 +0100

    Remove postTypes interface

--- client/src/features/posts/types/postTypes.ts
+++ client/src/features/posts/types/postTypes.ts
@@ -9,35 +9,6 @@ export interface postDataTypes {
   isEdited: boolean;
 }
 
-export interface postTypes {
-  id: number;
-  index: number;
-  postData: postDataTypes;
-  postOpacity: boolean[];
-  //PostOpacity array contains opacity state for each post, which is used to control the animation effect for displaying
-  enableOptions?: boolean;
-  deleteModeEnable?: boolean;
-  updateModeEnable: boolean;
-  handleDeleteModeEnable: (index: number) => void;
-  handleDeletePost: (
-    id: number,
-    index: number,
-    fetchPostsUsername: string //Variable is used to fetch only user's posts after post deletion
-  ) => void;
-  // "id" is the unique identifier in database
-  // "index" is position of the post in the list
-  toggleUpdateMode: (index: number) => void;
-  handleSetNewPostContentData: ({
-    newText,
-    newImage,
-  }: postContentToUpdateTypes) => void;
-  handleUpdatePost: (
-    postIdToUpdate: number,
-    index: number,
-    fetchPostsUsername: string //Variable is used to fetch only user's posts after post update
-  ) => void;
-}
-
 export interface postContentDataTypes {
   text: string;
   image?: File;

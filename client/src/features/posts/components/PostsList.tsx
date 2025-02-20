e77d6a5b98f8da6f125b404728d174a08668faa0
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2025-02-20 18:52:13 +0100

    Refactor code structure

--- client/src/features/posts/components/PostsList.tsx
+++ client/src/features/posts/components/PostsList.tsx
@@ -1,31 +1,21 @@
 import { useEffect } from "react";
 
-import PostSkeleton from "./post/PostSkeleton";
-import PostLayout from "./post/PostLayout";
+import PostSkeleton from "./post/postLayout/PostSkeleton";
+import Post from "./post/postLayout/Post";
 import useListPosts from "../hooks/useListPosts";
-import useRemovePost from "../hooks/useRemovePost";
+// import useRemovePost from "../hooks/useDeletePost";
 import { postDataTypes } from "../types/postTypes";
 import { usePostsContext } from "../../../context/PostsContext";
-import useUpdatePost from "../hooks/useUpdatePost";
+// import useUpdatePost from "../hooks/useUpdatePost";
 
 const PostsList = ({
   displayByUser,
   enableOptions,
 }: {
   displayByUser?: string;
-  enableOptions: boolean;
+  enableOptions?: boolean;
 }) => {
-  const { loading, postsData, postOpacity, queryError } = useListPosts();
-
-  const { deleteModeEnable, handleDeletePost, handleDeleteModeEnable } =
-    useRemovePost(postsData);
-
-  const {
-    updateModeEnable,
-    toggleUpdateMode,
-    handleSetNewPostContentData,
-    handleUpdatePost,
-  } = useUpdatePost(postsData);
+  const { loading, postsData, queryError } = useListPosts();
 
   const { handleFetchingPosts } = usePostsContext();
 
@@ -48,24 +38,8 @@ const PostsList = ({
         </div>
       ) : (
         <>
-          {postsData.map((postData: postDataTypes, index: number) => (
-            <PostLayout
-              id={postData.id}
-              key={index}
-              index={index}
-              postData={postData}
-              postOpacity={postOpacity}
-              enableOptions={enableOptions}
-              //post delete
-              deleteModeEnable={deleteModeEnable[index]} //enabling delete mode for specified post, by index
-              handleDeleteModeEnable={handleDeleteModeEnable}
-              handleDeletePost={handleDeletePost}
-              //post update
-              updateModeEnable={updateModeEnable[index]} //enabling edit mode for specified post, by index
-              toggleUpdateMode={toggleUpdateMode}
-              handleSetNewPostContentData={handleSetNewPostContentData}
-              handleUpdatePost={handleUpdatePost}
-            />
+          {postsData.map((postData: postDataTypes) => (
+            <Post postData={postData} enableOptions={enableOptions} />
           ))}
         </>
       )}

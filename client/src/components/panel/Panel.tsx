0257bb3efdab48f2ef65f4e4f9ac07cdce6377b5
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2025-02-21 06:35:12 +0100

    Fix: Apply flexbox to close button

--- client/src/components/panel/Panel.tsx
+++ client/src/components/panel/Panel.tsx
@@ -21,7 +21,7 @@ const Panel = ({
             <div className="sticky my-[4vh] overflow-auto border-t max-h-[100vh]  border-neutral-700 bg-neutral-900 rounded-xl text-white min-h-[200px] max-w-[300px] sm:max-w-none xs:min-w-[400px] p-4 h-fit">
               <div className="absolute flex justify-end  top-1.5 right-1.5 transition-all ease-in duration-2">
                 <button
-                  className="bg-red-950 border-t-[0.5px] border-red-900 w-5 h-5 hover:border-red-400  hover:bg-red-500 transition-all rounded-md p-0.5 ease-in duration-25"
+                  className="bg-red-950 border-t-[0.5px] flex justify-center items-center border-red-900 w-5 h-5 hover:border-red-400  hover:bg-red-500 transition-all rounded-md p-0.5 ease-in duration-25"
                   onClick={closePanel}
                 >
                   <img src={closeIcon} />

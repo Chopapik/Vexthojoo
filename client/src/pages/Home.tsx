0b10dc5fbb50e94695caab4a319d78405090f779
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2025-02-22 18:21:40 +0100

    feat: integrate mobileNavigator into Home component

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -7,10 +7,15 @@ import avatarNull from "../assets/images/avatarNull.png";
 import defaultAvatar from "../assets/images/defaultAvatar.png";
 import forwardIcon from "../assets/icons/forwardIcon.svg";
 
+import MobileNavigator from "../components/layout/MobileNavigator";
+import useMobileNavigation from "../hooks/mobileNavigator/useMobileNavigation";
+
 const Home = () => {
   const { showPanel } = usePanelContext();
 
   const { authData } = useCookieAuthContext();
+
+  const { scrollToTop, toggleAside, isAsideVisible } = useMobileNavigation();
 
   return (
     <>
@@ -20,7 +25,7 @@ const Home = () => {
 
       <div
         id="content"
-        className="w-full flex flex-col items-center max-w-[650px] lg:max-w-[1440px] lg:flex-row lg:items-start space-x-3"
+        className="w-full flex flex-col items-center max-w-[650px] lg:max-w-[1440px] lg:flex-row lg:items-start lg:space-x-3"
       >
         <main className="w-full lg:flex-1 flex flex-col items-center space-y-4">
           {/* add post input */}
@@ -52,21 +57,41 @@ const Home = () => {
           <hr className="w-full max-w-[910px] border-neutral-700" />
 
           <PostsList enableOptions={false} />
+          <MobileNavigator
+            upButtonFunction={scrollToTop}
+            moreButtonFunction={toggleAside}
+          />
         </main>
-        <aside className="w-full lg:w-[300px] xl:w-[400px] lg:sticky lg:top-[calc(70px+12px+12px)] lg:h-[calc(100vh-12px-12px-12px-70px)] flex flex-col items-end bg-[#080808] border-t border-[#171717] rounded-xl">
-          <footer className="mt-auto h-fit flex text-xs flex-col px-5 py-3 items-center space-y-2 w-full font-roboto font-bold">
-            <div className="text-neutral-300 space-x-2 flex justify-center items-center">
+
+        <aside
+          className={`
+              rounded-xl z-50 bg-[#0B0B0B] fixed max-w-[400px] lg:sticky flex justify-between
+              w-[calc(100vw-12px-12px)] 
+              h-[calc(100vh-12px-12px-12px-37px)]
+              top-3
+              lg:top-[calc(12px+70px+12px)]
+              lg:h-[calc(100vh-12px-70px-12px-12px)]
+              transition-all ease-in-out duration-500
+
+              ${isAsideVisible ? "right-3" : "-right-[100vw]"}
+
+            
+            `}
+        >
+          test
+          <footer className="mt-auto h-fit flex flex-col items-center space-y-2 w-full px-5 py-3 text-xs font-roboto font-bold">
+            <div className="flex items-center justify-center space-x-2 text-neutral-300">
               <a href="/listaUzytkownikow" className="hover:underline">
                 Lista użytkowników
               </a>
-              <span className="w-1 h-1 rounded-full bg-neutral-300 inline-block"></span>
+              <span className="inline-block h-1 w-1 rounded-full bg-neutral-300"></span>
               <a href="/coNowego" className="hover:underline">
                 Co nowego
               </a>
             </div>
-            <hr className="w-full border-neutral-700 " />
-            <span className="text-neutral-700 font-extrabold">
-              &copy;Chopapik 2024-2025
+            <hr className="w-full border-neutral-700" />
+            <span className="font-extrabold text-neutral-700">
+              &copy; Chopapik 2024-2025
             </span>
           </footer>
         </aside>

14b329554171cfdaf7d8886f52bfc32db96b42de
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2025-02-28 07:00:00 +0100

    Refactor: Update sql query

--- api/controllers/posts/fetchPosts.ts
+++ api/controllers/posts/fetchPosts.ts
@@ -5,7 +5,7 @@ import { Request, Response } from "express";
 const fetchPosts = async (req: Request, res: Response) => {
   try {
     const [posts] = await db.query(
-      "SELECT p.id,u.username,u.avatarPath,p.text,p.whenUpload,p.whatDevice,p.imagePath,p.isEdited FROM users u JOIN posts p ON p.user_id=u.id ORDER BY p.whenUpload DESC"
+      "SELECT p.*,u.username,u.avatarPath FROM users u JOIN posts p ON p.user_id=u.id ORDER BY p.whenUpload DESC"
     );
 
     res.status(200).json(posts);

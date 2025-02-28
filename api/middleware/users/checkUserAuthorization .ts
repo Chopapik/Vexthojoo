e8a3abfe74f04d690bdc79aca9a9a95dd4310c8e
Krzysztof Babik <123145310+Chopapik@users.noreply.github.com>
2025-02-28 06:46:44 +0100

    Refactor: Remove redundant function
    Remove token validation and decoding functions

--- api/middleware/users/checkUserAuthorization .ts
+++ api/middleware/users/checkUserAuthorization .ts
@@ -5,36 +5,25 @@ const checkUserAuthorization = (
   getUserId: (req: Request, res: Response) => number | Promise<number>
 ) => {
   return async (req: Request, res: Response, next: NextFunction) => {
-    const token = req.cookies.token;
-    const secret = process.env.secret;
+    const decodedToken = req.body.decodedToken;
 
-    if (!token) {
-      res.status(401).json({ message: "No token provided" });
-    } else if (!secret) {
-      res.status(401).json({ message: "No secret provided" });
-    } else {
-      try {
-        const foundUserId =
-          getUserId(req, res) instanceof Promise
-            ? await getUserId(req, res)
-            : getUserId(req, res);
+    const { userid } = decodedToken;
 
-        const decodedToken = jwt.verify(token, secret);
-        const decodedUserid = (decodedToken as jwt.JwtPayload).userid;
+    try {
+      const foundUserId =
+        getUserId(req, res) instanceof Promise
+          ? await getUserId(req, res)
+          : getUserId(req, res);
 
-        console.log("token id: ", decodedUserid);
-        console.log("userid", foundUserId);
-
-        if (decodedUserid === foundUserId) {
-          next();
-        } else {
-          res
-            .status(500)
-            .json({ message: "User is not authorized to perform this action" });
-        }
-      } catch (error) {
-        res.status(401).json({ message: "Invalid token" });
+      if (userid === foundUserId) {
+        next();
+      } else {
+        res
+          .status(500)
+          .json({ message: "User is not authorized to perform this action" });
       }
+    } catch (error) {
+      res.status(401).json({ message: "Invalid token" });
     }
   };
 };
